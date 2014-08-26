a95d6ae347338303551078a46f0bd0647ff6a641
Nick Roberts <nickrhys@me.com>
2014-08-26 16:33:12 +0100

    test one passed, test two created

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,11 +1,15 @@
-
+require 'board'
 
 describe Board do
 
 	it "should know that it is a board" do
 		board = Board.new
-		expect(board).to be Board
+		expect(board).to be_a Board
 
 	end
+
+	it "should have a 100 cells" do
+		expect(board.cells.count).to eq 100
+	end 
 
 end
