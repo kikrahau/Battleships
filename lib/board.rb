c5668339980dd4392dd38a58cd5275cad10b3e0a
ruthearle <rrearle@gmail.com>
2014-08-26 17:29:23 +0100

    second test passed - cell method created. Third test written - failed

--- lib/board.rb
+++ lib/board.rb
@@ -1,5 +1,13 @@
 class Board
 
 
+  def initialize
+
+  end
+
+  def cells
+    @cells ||= Array.new(100)
+
+  end
 
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,22 +1,22 @@
-<<<<<<< HEAD
 require 'board'
 
-=======
->>>>>>> da1b04b9dc2528119521712eb30254ab05fafc4d
 describe Board do
 
+  let(:board) { Board.new     }
+  let(:water) { double :water }
+
 	it "should know that it is a board" do
-		board = Board.new
 		expect(board).to be_a Board
 
 	end
 
-<<<<<<< HEAD
 	it "should have a 100 cells" do
 		expect(board.cells.count).to eq 100
-	end 
+	end
+
+  it 'all cells should be initialized containing water' do
+    expect(board.cells).to be_a Water
+  end
+
 
 end
-=======
-end
->>>>>>> da1b04b9dc2528119521712eb30254ab05fafc4d
