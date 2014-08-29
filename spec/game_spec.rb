80db6e45d61d8f6764bfd49e1b2064155310d9bc
michballard <michballard@gmail.com>
2014-08-29 11:50:51 +0100

    Players names added

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -4,26 +4,52 @@ require 'game'
 describe Game do 
 
 	let(:game) { Game.new }
-	let(:player1) { double :player }
+	let(:player1) { double :player}
 	let(:player2) { double :player }
 
-	it 'should not be initialized with players' do
-		expect(game.players).to eq []
+
+	context 'initialization' do 
+
+		it 'should not be initialized with players' do
+			expect(game.players).to eq []
+		end
+
+		it 'should be able to add players' do
+			game.add!(player1)
+			expect(game.players).to eq [player1]
+		end
+
+		it 'should be able to know, when to start' do
+			expect(game).to be_starting
+		end
+
+		it "should be able to start, when there are two players" do
+			expect(game.starting?).to be true
+		end
+
+		it "should ask player 1 for their name" do 
+			allow(game).to receive(:gets).and_return("Nick")
+			expect(game.get_name_player_1).to eq "Nick"
+		end
+
+		it "should raise error if player 1 doesn't enter a name" do
+			allow(game).to receive(:gets).and_return("\n")
+			expect(game.get_name_player_1).to eq("Player 1") 
+		end 
+
+		it "should ask player 2 for their name" do 
+			allow(game).to receive(:gets).and_return("Merve")
+			expect(game.get_name_player_2).to eq "Merve"
+		end
+
+		it "should raise error if player 2 doesn't enter a name" do
+			allow(game).to receive(:gets).and_return("\n")
+			expect(game.get_name_player_2).to eq("Player 2") 
+		end 
+
 	end
 
-	it 'should be able to add players' do
-		game.add!(player1)
-		expect(game.players).to eq [player1]
-	end
 
-
-	it 'should be able to know, when to start' do
-		expect(game).to be_starting
-	end
-
-	it "should be able to start, when there are two players" do
-		expect(game.starting?).to be true
-	end
 
 	it 'should know who is the current player' do
 		game.add!(player1)
